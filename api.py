323dfe883e9c1051886609b06315e032f7067154
Phan Ngọc Tài <ngoctaiphan.cv@gmail.com>
2026-07-04 15:56:56 +0700

    chore: optimize bytecode writing in api.py

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 # FastAPI backend root entrypoint wrapper
-import uvicorn
 import os
+os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
+import sys
+sys.dont_write_bytecode = True
+import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
